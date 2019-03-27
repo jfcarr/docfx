6b9712ea76dd10d0f61790d2ffffc141d94043c4
Jim Carr <hidden-email>
2019-03-27 14:16:54 -0400

    First draft of service_doc

---
+++ service_doc/src/Model/TestModel.cs
@@ -0,0 +1,11 @@
+namespace src.Model
+{
+    public class TestModel
+    {
+		/// <summary>
+		/// This is the Model ID.
+		/// </summary>
+		/// <value></value>
+        public string  ModelId {get; set;}
+    }
+}
